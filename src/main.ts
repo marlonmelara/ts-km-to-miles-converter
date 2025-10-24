3caecac939a1ed2cf6e79a621ed5efb9224afbcf
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2025-10-24 18:46:05 -0300

    [INIT] Setup TypeScript project structure and HTML

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+// Convertir de kilómetros a millas, sabiendo que: (1 kilómetro es igual a 0.62 millas )
+// Ejemplo: 10 kilómetros = 6.21371 millas
+
+const KM_TO_MILE_FACTOR: number = 0.621371;
+
+function convertKMtoMI(km: number): string {
+  if (km <= 0) {
+    return 'Tienes que poner un número positivo';
+  }
+
+  const miles = km * KM_TO_MILE_FACTOR;
+  return `${km} kilómetros es igual a ${miles.toFixed(2)} millas`;
+}
+
+// ✅ Esperamos a que el DOM esté listo
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('fileForm') as HTMLFormElement;
+
+  form.addEventListener('submit', function (e) {
+    // ✅ Prevenimos el comportamiento por defecto del formulario
+    e.preventDefault();
+
+    const kmInput = document.getElementById('kmInput') as HTMLInputElement;
+    const resultOutput = document.getElementById(
+      'resultOutput'
+    ) as HTMLDivElement;
+
+    const km: number = parseFloat(kmInput.value);
+
+    resultOutput.textContent = convertKMtoMI(km);
+  });
+});
+// convertKMtoMI(10);
